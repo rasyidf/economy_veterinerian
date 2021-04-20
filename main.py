967227fe7515b6f994c0615469ee43a27323cf59
Muhammad Fahmi Rasyid <ufarasfa@gmail.com>
2021-04-20 21:29:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ pilihan4 = st.sidebar.checkbox("B/C Ratio")
 if pilihan1:
     st.markdown("## Future Value")
     col_A, col_B, col_C = st.beta_columns(3)
-    pv = col_B.number_input("Present Value", key="fv_1") # NP
+    pv = col_A.number_input("Present Value", key="fv_1") # NP
     i = col_B.number_input("i", key="fv_2")
     n = col_C.number_input("n", key="fv_3")
     runs = col_B.button("Hitung", key="fv_4")
@@ -44,40 +44,39 @@ if pilihan3:
     ynn = []
     st.markdown("## Nett Present Value\n\n Tuliskan Input Xn dalam koma")
     xn :  str = st.text_input("Xn (tahun 1, tahun 2, tahun 3, ...)", key="nv_1")
-    yn :  str = st.text_input("Xn (tahun 1, tahun 2, tahun 3, ...)", key="nv_2")
-    
-    if xn:
-        xn = xn.replace(" ", "")
-        xnn =[int(x) for x in xn.split("-")]
-    if yn:
-        yn = yn.replace(" ", "")
-        ynn =[int(y) for y in yn.split("-")]
+    yn :  str = st.text_input("Yn (tahun 1, tahun 2, tahun 3, ...)", key="nv_2")
+     
+     
 
-    r = st.number_input("r", key="nv_3") 
-    col1, col2 = st.beta_columns(2)  
+    r = st.number_input("r", key="nv_3")  
  
     hari = st.number_input("Hari", value = -1, key="nv_4") 
     tahun = st.number_input("Tahun", value = -1, key="nv_5")
 
-    runs = st.button("Hitung", key="nv_6")
+    runs = st.button("Hitung", key="nv_6")  
     if runs:
-        hasil, interpretasi = epd.NPV(xnn, ynn, r, hari, tahun)
+        xn = xn.replace(" ", "")
+        xnn =[int(x) for x in xn.split(",")] 
+        yn = yn.replace(" ", "")
+        ynn =[int(y) for y in yn.split(",")]
+        hasil, interpretasi, ix, iy = epd.NPV(xnn, ynn, r)
         st.markdown("## Nilai") 
         st.write(hasil)
+
         st.markdown("## Interpretasi") 
+        col1, col2 = st.beta_columns(2) 
+        col1.markdown(ix)  
+        col2.markdown(iy) 
         st.write(interpretasi)
     else:
         st.markdown(str_isi_semua)
     
 if pilihan4:
     st.markdown("## B/C Ratio")
-    fv = st.number_input("Future Value") # NP
-    i = st.number_input("r")
-    n = st.number_input("n")
+    TPV_B = st.number_input("Benefits")
+    TPB_C = st.number_input("Cost")
     runs = st.button("Hitung")
     if runs:
-        TPV_B = st.number_input("Benefits")
-        TPB_C = st.number_input("Cost")
         hasil, interpretasi = epd.bc_ratio(TPV_B,TPB_C)
         st.markdown("## Nilai") 
         st.write(hasil)
