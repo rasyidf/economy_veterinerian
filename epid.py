5ea918cd5538cf6a3b280d24fb5c344fd5893f6b
rasyidf <ufarasfa@gmail.com>
2021-04-21 01:46:19 +0700

    Update lagi

--- epid.py
+++ epid.py
@@ -1,3 +1,6 @@
+import streamlit as st
+
+
 class Epid:
     def future_val(self, pv, i, n):
         return pv*(1+i)**n
@@ -5,49 +8,44 @@ class Epid:
     def present_val(self, fv, r, n):
         return fv/(1+r)**n
 
-    def npv(self, xn=[], yn=[], r=0.0, n=1, hari=-1, tahun=-1):
+    def npv(self, xn, yn, r=0.05,  hari=-1, tahun=-1):
+        nv = "tahun"
+        np = tahun
         if hari > 0:
             n = round(hari / 365, 2)
+            nv = "hari"
+            np = hari
         elif tahun > 0:
             n = tahun
         else:
             print("tahun atau hari wajib diisi")
 
         interpretasi = ""
-        TPVX = 0
-        PVXS = []
-        for xi in xn:
-            vals = self.present_val(xi, r, n)
-            PVXS.append(vals)
-            TPVX += vals
-            interpretasi += f"PV B tahun = {xi}/(1+{r})^{n}\n\n"
-            interpretasi += f"= {vals}\n\n"
-        
 
+        benefits = self.present_val(xn, r, n)
+        cost = self.present_val(yn, r, n)
 
-        TPVY = 0
-        PVYS = []
-        for yi in yn:
-            vals = self.present_val(yi, r, n)
-            PVYS.append(vals)
-            TPVY += vals
-            interpretasi += f"PV C tahun = {yi}/(1+{r})^{n}\n\n"
-            interpretasi += f"= {vals}\n\n"
+        NPV = benefits - cost
+        st.markdown(f"Benefits : `{benefits}`")
+        st.markdown(f"Cost : `{cost}`")
+        if NPV > 0:
+            int_BC = "keuntungan"
+        elif NPV < 0:
+            int_BC = "kerugian"
+        else:
+            int_BC = "Breakeven Point"
 
-        # NPV result = TPV Benefit - TPV Cost
-        NPV = TPVX - TPVY
-        interpretasi += f"Jadi, peternakan sapi tersebut memperoleh keuntungan sebesar Rp{NPV}/ekor selama {hari} hari/bulan/tahun\n\n"
+        interpretasi += f"Jadi, peternakan sapi tersebut memperoleh {int_BC} sebesar Rp{NPV}/ekor selama {np} {nv}\n\n"
+        return NPV, interpretasi, benefits, cost
 
-        return NPV, interpretasi
-    
-    def NPV(self, xn=[], yn=[], r=0.0, year = 2010):
-        
+    def NPV(self, xn=[], yn=[], r=0.0, year=2010):
+
         ix = ""
         iy = ""
         interpretasi = ""
         TPVX = 0
         PVXS = []
-        n = 0 
+        n = 0
         # TPVBenefit
         for xi in xn:
             n += 1
@@ -56,12 +54,10 @@ class Epid:
             TPVX += vals
             ix += f"PV B {year + n-1} = {xi}/(1+{r})^{n}\n\n"
             ix += f"= {vals}\n\n"
-        
-
 
         TPVY = 0
         PVYS = []
-        n = 0 
+        n = 0
         for yi in yn:
             n += 1
             vals = self.present_val(yi, r, n)
@@ -72,8 +68,8 @@ class Epid:
 
         # NPV result = TPV Benefit - TPV Cost
         NPV = TPVX - TPVY
-        ix += f"TPV Benefit : {TPVX} \n\n" 
-        iy += f"TPV Cost : {TPVY} \n\n" 
+        ix += f"TPV Benefit : {TPVX} \n\n"
+        iy += f"TPV Cost : {TPVY} \n\n"
         int_BC = ""
         if NPV > 0:
             int_BC = "keuntungan"
@@ -84,7 +80,7 @@ class Epid:
 
         interpretasi += f"Jadi, peternakan sapi tersebut memperoleh {int_BC} sebesar Rp{NPV}/ekor selama {n} tahun\n\n"
 
-        return NPV, interpretasi, ix , iy
+        return NPV, interpretasi, ix, iy, TPVX, TPVY
 
     def bc_ratio(self, tpvx, tpvy):
         ratio = tpvx/tpvy
--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import streamlit as st
-from epid import Epid 
+from epid import Epid
 epd = Epid()
+
+hasil_1 = 0
+hasil_2 = 0
+interpretasi_1 = ""
+interpretasi_2 = ""
+benefits = 0
+cost = 0 
 
 str_isi_semua = "> Isi semuanya lalu tekan hitung "
 st.title('Epid 8.0: Ekonomi Veteriner')
@@ -9,74 +16,90 @@ st.markdown('Aplikasi ini buat ngitung apa gitu (Bikinan PROF Paus & DRH Biru)')
 
 st.sidebar.markdown("## Pilihan")
 pilihan1 = st.sidebar.checkbox("Future Value")
-pilihan2 = st.sidebar.checkbox("Present Value")
-pilihan3 = st.sidebar.checkbox("Nett Present Value")
-pilihan4 = st.sidebar.checkbox("B/C Ratio")
+pilihan2 = st.sidebar.checkbox("Present Value") 
 
 if pilihan1:
     st.markdown("## Future Value")
     col_A, col_B, col_C = st.beta_columns(3)
-    pv = col_A.number_input("Present Value", key="fv_1") # NP
+    pv = col_A.number_input("Present Value", key="fv_1")  # NP
     i = col_B.number_input("i", key="fv_2")
     n = col_C.number_input("n", key="fv_3")
     runs = col_B.button("Hitung", key="fv_4")
     if runs:
-        hasil = epd.future_val(pv,i, n)
-        st.markdown("## Nilai Future Value") 
+        hasil = epd.future_val(pv, i, n)
+        st.markdown("## Nilai Future Value")
         st.write(hasil)
     else:
         st.markdown(str_isi_semua)
 if pilihan2:
     st.markdown("## Present Value")
     col_A, col_B, col_C = st.beta_columns(3)
-    fv = col_A.number_input("Future Value", key="pv_1") # NP
+    fv = col_A.number_input("Future Value", key="pv_1")  # NP
     r = col_B.number_input("i", key="pv_2")
     n = col_C.number_input("n", key="pv_3")
     runs = st.button("Hitung", key="pv_4")
     if runs:
-        hasil = epd.present_val(fv,r, n)
-        st.markdown("## Present Value") 
+        hasil = epd.present_val(fv, r, n)
+        st.markdown("## Present Value")
         st.write(hasil)
     else:
         st.markdown(str_isi_semua)
 
+st.markdown("## Nett Present Value [Single]")
+xn:  int = st.number_input("Total V Benefits", key="nnv_1", step=1, min_value=0, max_value=10000000000, value=10000)
+yn:  int = st.number_input("Total V Cost", key="nnv_2", step=1, min_value=0, max_value=10000000000, value=10000)
+
+hari = st.number_input("Hari", value=-1, key="nnv_4")
+tahun = st.number_input("Tahun", value=-1, key="nnv_5")
+
+r = st.number_input("R", key="nnv_7", value=0.07)
+
+runs = st.button("Hitung", key="nnv_6")
+if runs:
+    hasil_1, interpretasi_1, benefits, cost = epd.npv(
+        xn, yn, r, hari=hari, tahun=tahun)
+    st.markdown("### Nilai")
+    st.write(hasil_1)
+
+    st.markdown("### Interpretasi") 
+    st.write(interpretasi_1)
+    
+    hasil, interpretasi = epd.bc_ratio(benefits, cost) 
+    st.write(interpretasi)
+else:
+    st.markdown(str_isi_semua)
+
+
 xnn = []
 ynn = []
-st.markdown("## Nett Present Value\n\n Tuliskan Input Xn dalam koma")
-xn :  str = st.text_input("Xn (tahun 1, tahun 2, tahun 3, ...)", key="nv_1")
-yn :  str = st.text_input("Yn (tahun 1, tahun 2, tahun 3, ...)", key="nv_2")
-    
-tahun = st.number_input("Tahun", key="nv_3", min_value= 1000, max_value=3000, value=2019, step=1)  
+st.markdown("## Nett Present Value [Banyak]\n\n Tuliskan Input Xn dalam koma")
+xn:  str = st.text_input("Xn (tahun 1, tahun 2, tahun 3, ...)", key="nv_1")
+yn:  str = st.text_input("Yn (tahun 1, tahun 2, tahun 3, ...)", key="nv_2")
 
-r = st.number_input("R", key="nv_7")  
+tahun = st.number_input("Tahun", key="nv_3", min_value=1000,
+                        max_value=3000, value=2019, step=1)
 
-runs = st.button("Hitung", key="nv_6")  
+r = st.number_input("R", key="nv_7")
+
+runs = st.button("Hitung", key="nv_6")
 if runs:
     xn = xn.replace(" ", "")
-    xnn =[int(x) for x in xn.split(",")] 
+    xnn = [int(x) for x in xn.split(",")]
     yn = yn.replace(" ", "")
-    ynn =[int(y) for y in yn.split(",")]
-    hasil, interpretasi, ix, iy = epd.NPV(xnn, ynn, r, year=tahun)
-    st.markdown("## Nilai") 
+    ynn = [int(y) for y in yn.split(",")]
+    hasil, interpretasi, ix, iy, TPV_B, TPV_C = epd.NPV(xnn, ynn, r, year=tahun)
+    st.markdown("### Nilai")
     st.write(hasil)
 
-    st.markdown("## Interpretasi") 
-    col1, col2 = st.beta_columns(2) 
-    col1.markdown(ix)  
-    col2.markdown(iy) 
+    st.markdown("### Interpretasi")
+    col1, col2 = st.beta_columns(2)
+    col1.markdown(ix)
+    col2.markdown(iy)
+    st.write(interpretasi)
+    hasil, interpretasi = epd.bc_ratio(TPV_B, TPB_C)
+    st.markdown("### Nilai")
+    st.write(hasil) 
     st.write(interpretasi)
 else:
     st.markdown(str_isi_semua)
-    
-st.markdown("## B/C Ratio")
-TPV_B = st.number_input("Benefits")
-TPB_C = st.number_input("Cost")
-runs = st.button("Hitung")
-if runs:
-    hasil, interpretasi = epd.bc_ratio(TPV_B,TPB_C)
-    st.markdown("## Nilai") 
-    st.write(hasil)
-    st.markdown("## Interpretasi") 
-    st.write(interpretasi)
-else:
-    st.markdown(str_isi_semua)
+ 
